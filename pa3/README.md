4140d5566eb2518e1707d57152b9ad5d756a4477
Jae Park <96jpark@gmail.com>
2021-12-04 17:54:25 -0500

    pa3

--- pa3/README.md
+++ pa3/README.md
@@ -2,25 +2,34 @@
 
 http://cs.rpi.edu/academics/courses/fall21/csci4430/pa3/pa3.pdf
 
-install Oz 1.4.0 from  https://github.com/mozart/mozart  to run the files
+SWI-Prolog version 7.6.4 for amd64
 
 ### Contributors
 Jae Park and Roland Rao
 
 ### Features
-1. 
-2. 
+#### part 1
+1. Parses out the information of a family tree from the given text file
+2. Using constraints & rules, find Siblings, first cousins, list ancestors, list descendants, etc
+
+#### part 2
+1. read in definitions of processors and constraints on microprocessor attributes (NLP)
+2.  run a constraint computation to calculate how many of each processor should 
+be placed on a common chip
+3. return all possible combinations of different processor copnfigurations that satisfy the constraints
 
 ### Bugs (as of Nov 16, 2021)
-1. 
+1. No bugs found as of submission 12/4
 
 
 ### Run 
 
 ```bash
-$ 
+$ swipl -f part1_tester.pl -g main
+
+$ swipl -f part2.pl -g main < procSpec1.txt
+$ swipl -f part2.pl -g main < procSpec2.txt
 ```
 
 ### Notes
-
 
--- pa3/prolog/part2.pl
+++ pa3/prolog/part2.pl
@@ -1,98 +1,109 @@
+:- use_module(library(clpfd)).
 :- [read_line].
-% listmember(X,[X|]).
-% listmember(X,[|TAIL]) :- list_member(X,TAIL).
 
-% list_append(A,T,T) :- list_member(A,T),!.
-% list_append(A,T,[A|T]).
-
-% print_list([]) :- writeln("").
-% print_list([H|TAIL]) :- print_list(TAIL), write(H).
-
-
-% how is this func working
-read_loop(P, C) :-
+read_loop(IDs, Cores, Areas, Costs, CoreC, AreaC, CostC) :-
 	read_line(Line),
 	length(Line, Len),
 	(
 		Len > 0,
-		write(Line), nl,
 		sentence(T, Line, []),
-		writeln("Before if"),
-		(length(T, Length), Length =:= 4 ->  
-			append(C, [], NC),
-			append(P, [T], NP);
-			append(C, [T], NC),
-			append(P, [], NP)
+		(length(T, Length), Length =:= 4 -> 
+			nth0(0, T, First),
+			nth0(1, T, Second),
+			nth0(2, T, Third),
+			nth0(3, T, Fourth), 
+			append(IDs, [First], NIDs), 
+			append(Cores, [Second], NCores),
+			append(Areas, [Third], NAreas),
+			append(Costs, [Fourth], NCosts),
+			append(CoreC, [], NCoreC),
+			append(AreaC, [], NAreaC),
+			append(CostC, [], NCostC);
+			nth0(0, T, Attribute),
+			(Attribute = core -> append(CoreC, T, NCoreC), append(AreaC, [], NAreaC), append(CostC, [], NCostC); true),
+			(Attribute = area -> append(AreaC, T, NAreaC), append(CoreC, [], NCoreC), append(CostC, [], NCostC); true),
+			(Attribute = cost -> append(CostC, T, NCostC), append(CoreC, [], NCoreC), append(AreaC, [], NAreaC); true),
+			append(IDs, [], NIDs),
+			append(Cores, [], NCores),
+			append(Areas, [], NAreas),
+			append(Costs, [], NCosts)
 		),
-		writeln(NP),
-		writeln(NC),
-		read_loop(NP, NC);
+		read_loop(NIDs, NCores, NAreas, NCosts, NCoreC, NAreaC, NCostC);
 		Len =< 0,
-		write('Read last line.'),
-		optimize(P, C),
-		% convert_constraints(C),
+		findall(R, optimize(Cores, Areas, Costs, CoreC, AreaC, CostC, R), Rs),
+		print_recursive(Rs, IDs),
+		true
+	).
+
+print_recursive(Rs, IDs) :-
+	length(Rs, Length),
+	(
+		Length > 0,
+		Rs = [H | T],
+		print_inner_recursive(H, IDs),
+		print_recursive(T, IDs);
+		true
+	).
+
+print_inner_recursive(Row, IDs) :-
+	length(Row, RLength),
+	(
+		RLength > 1,
+		Row = [H | T],
+		IDs = [HID | TID],
+		write(HID),
+		write(" = "),
+		write(H),
+		write(", "),
+		print_inner_recursive(T, TID);
+		RLength = 1,
+		Row = [H | T],
+		IDs = [HID | TID],
+		write(HID),
+		write(" = "),
+		write(H),
+		print_inner_recursive(T, TID);
+		write(";"),
 		nl
 	).
 
-
 main :-
-	P = [],
-	C = [],
-	read_loop(P, C).
+	IDs = [], Cores = [], Areas = [], Costs = [], CoreC = [], AreaC = [], CostC = [],
+	read_loop(IDs, Cores, Areas, Costs, CoreC, AreaC, CostC).
 
-optimize(NP, NC) :-
-	length(NP, Length),
-	(
-		
-	)
+optimize(Cores, Areas, Costs, CoreC, AreaC, CostC, Results) :-
+	length(Cores, LengthVal),
+	length(Results, LengthVal),
+	Results ins 0..16,
+	nth0(1, CoreC, CoreComp),
+	nth0(2, CoreC, CoreVal),
+	nth0(1, AreaC, AreaComp),
+	nth0(2, AreaC, AreaVal),
+	nth0(1, CostC, CostComp),
+	nth0(2, CostC, CostVal),
+	(CoreComp = greater -> scalar_product(Cores, Results, #>, CoreVal); true),
+	(CoreComp = less -> scalar_product(Cores, Results, #<, CoreVal); true),
+	(CoreComp = equal -> scalar_product(Cores, Results, #=, CoreVal); true),
+	(integer(CoreComp) -> scalar_product(Cores, Results, #>=, CoreComp), scalar_product(Cores, Results, #=<, CoreVal); true),
+	(AreaComp = greater -> scalar_product(Areas, Results, #>, AreaVal); true),
+	(AreaComp = less -> scalar_product(Areas, Results, #<, AreaVal); true),
+	(AreaComp = equal -> scalar_product(Areas, Results, #=, AreaVal); true),
+	(integer(AreaComp) -> scalar_product(Areas, Results, #>=, AreaComp), scalar_product(Areas, Results, #=<, AreaVal); true),
+	(CostComp = greater -> scalar_product(Costs, Results, #>, CostVal); true),
+	(CostComp = less -> scalar_product(Costs, Results, #<, CostVal); true),
+	(CostComp = equal -> scalar_product(Costs, Results, #=, CostVal); true),
+	(integer(CostComp) -> scalar_product(Costs, Results, #>=, CostComp), scalar_product(Costs, Results, #=<, CostVal); true),
+	label(Results).
 
-% cores(NP, Constraint) :-
-% 	nth0(0, Constraint, First),
-% 	nth0(1, Constraint, Second),
-% 	nth0(2, Constraint, Third),
-% 	(Second = less -> )
-
-% area() :-
-
-% cost() :-
-
-
-	
-% convert_constraints(NC) :-
-% 	length(NC, Len),
-% 	(
-% 		Len > 0,
-% 		NC = [H|T],
-% 		nth0(0, H, First),
-% 		nth0(1, H, Second),
-% 		nth0(2, H, Third),
-% 		(integer(Second) -> ;
-% 			(Second = less -> replace(less, 0, H, NewH)
-% 		)
-% 		replace(H, NewH, NC, ADF)
-% 		writeln(H),
-% 		convert_constraints(ADF);
-% 		Len =< 0,
-% 		write('Read last constraint.'),
-% 		nl
-% 	).
-
-
-
-% constraint(A, B, C, D, Processor, Constraints) :-
-	
-
-
-
-sentence([A, C, V]) --> attribute(A), imperative1, imperative2, be, comparison(C), throwaway, value(V), [.].
-sentence([A,V1,V2]) --> attribute(A), imperative1, imperative2, be, interval, value(V1), [to], value(V2), [.].
+sentence([A, C, V]) --> attribute(A), imperative1, imperative2, [be], comparison(C), throwaway, value(V), [.].
+sentence([A,V1,V2]) --> attribute(A), imperative1, imperative2, [be], interval, value(V1), [to], value(V2), [.].
 sentence([I, C, A, D]) --> [processor], [type], id(I), [has],
 	value(C), [cores], [,], [uses], value(A), [square], [centimeters], 
 	[,], [and], [costs], value(D), [dollars], [.].
 
 attribute(A) --> optional_article, constraint1(A), constraint2.
 optional_article --> [the] | [].
-constraint1(core) --> [core].
+constraint1(core) --> [core]. 
 constraint1(area) --> [area].
 constraint1(cost) --> [cost].
 constraint2 --> [count] | [].
@@ -101,8 +112,6 @@ id(I) --> [I].
 
 imperative1 --> [must] | [is].
 imperative2 --> [] | [to].
-
-be --> [be].
 
 comparison(equal) --> [equal].
 comparison(less) --> [less].
